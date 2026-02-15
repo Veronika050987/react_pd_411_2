b06cf41a4205a48b740ff360bd33df77c98a565d
Veronika050987 <veronicaviv@mail.ru>
2026-02-15 13:03:13 +0300

    Delete post DONE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from "react";
 import Header from './Header';
 import Hello from './Hello';
 import Length from './Length.js';
@@ -8,19 +9,23 @@ import Form from './Form.js';
 import Count from './Count.js';
 import Posts from './posts/Posts.js';
 
-let posts = 
-[
-  {id:'1', title:'C++ proc', content:'Процедурное программирование на языке C++'},
-  {id:'2', title:'C++ OOP', content:'Объектно-ориентированное программирование на языке C++'},
-  {id:'3', title:'Windows Desktop Development', content:'Разработка настольных приложений для ОС Windows'}
-]
-function removePost(id)
-{
-  alert(posts[id]);
-  posts = posts.filter(post => post.id !==id);
-}
+class App extends React.Component {
+  state = {
+    posts:
+      [
+        { id: '1', title: 'C++ proc', content: 'Процедурное программирование на языке C++' },
+        { id: '2', title: 'C++ OOP', content: 'Объектно-ориентированное программирование на языке C++' },
+        { id: '3', title: 'Windows Desktop Development', content: 'Разработка настольных приложений для ОС Windows' }
+      ]
+  }
+  removePost = (id) => {
+    alert(id);
+    //alert(this.state.posts[id]);
+    //posts = posts.filter(post => post.id !== id);
+    this.setState({posts: this.state.posts.filter(post => post.id !==id)});
+  }
 
-function App() {
+render() {
   return (
     <div className="App">
       {/* <Header />
@@ -29,9 +34,9 @@ function App() {
       <Range />
       <Form />
       <Count /> */}
-      <Posts posts = {posts} removePost={removePost}/>
+      <Posts posts={this.state.posts} removePost={this.removePost} />
     </div>
   );
 }
-
+}
 export default App;
